62d958855f5ef35a1755b4783d04e488a1a0500e
hiu <hieeupham123@gmail.com>
2026-03-15 00:12:40 +0700

    video 17

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -2,13 +2,13 @@
 // class component - giai đoạn 1
 import React from "react";
 class MyComponent extends React.Component {
+  state = { name: "Hiu", address: "Khánh Hòa", age: "22" };
   //JSX - Viết code js trong code html
   render() {
     // quy đinh nội dung component trả về
     return (
       <div>
-        My first component!
-        {Math.random()}
+        My name is: {this.state.name} and I am from {this.state.address}
       </div>
     );
   }
