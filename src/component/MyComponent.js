ec230ba07692648444ab66884e92df7551b21f10
hiu <hieeupham123@gmail.com>
2026-03-16 14:28:50 +0700

    Update lan 2

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -3,12 +3,40 @@
 import React from "react";
 class MyComponent extends React.Component {
   state = { name: "Hiu", address: "Khánh Hòa", age: "22" };
+  handleClick = (event) => {
+    //console.log(">> Clicked!");
+    console.log("Click mey button!");
+    this.setState({
+      name: "Eric",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
+  };
+  handleOnchange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
   //JSX - Viết code js trong code html
   render() {
     // quy đinh nội dung component trả về
     return (
       <div>
-        My name is: {this.state.name} and I am from {this.state.address}
+        My name is: {this.state.name} and I am {this.state.age}
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input type="text" onChange={(event) => this.handleOnchange(event)} />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
